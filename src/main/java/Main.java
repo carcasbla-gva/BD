faad22fe11b77b1762215f02ef6fb21ef72cba6b
carcasbla-gva <carcasbla@alu.edu.gva.es>
2026-02-09 10:33:12 +0100

    Conexión a la base de datos

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    // Es static porque pertenece a toda la clase y final porque no se va a modificar
+    static final java.sql.Connection con = DatabaseConnection.getConnection();
+    public static void main(String[] args) {
+        // No hace nada, de momento.
+        // En cada método que creemos, usaremos la variable `con`
+    }
+}
+
+
